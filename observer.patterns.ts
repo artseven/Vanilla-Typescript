055566a08cc12d140f13fc96c2bb98f425f44796
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-12-05 12:14:26 -0500

    observers finished

--- observer.patterns.ts
+++ observer.patterns.ts
@@ -36,6 +36,45 @@ class WeatherStation implements Subject {
     }
 }
 // observer
-class TemperatureDisplay {
+class TemperatureDisplay implements Observer {
 
+    private subject: Subject;
+
+    constructor(weatherStation: Subject) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+
+    public update(temperature: number) {
+        console.log('TemperatureDisplay: I need to update my display');
+        // Logic would go here.
+    }
 }
+
+// observer
+class Fan implements Observer {
+    
+    private subject: Subject;
+
+    constructor(weatherStation: Subject) {
+        this.subject = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+
+    public update(temperature: number) {
+        if (temperature > 25) {
+            console.log('Fan: its hot here, turning myself on.');
+        } else {
+            console.log('Fan: its nice and cool, turning myself off.');
+        }
+    }
+}
+
+let weatherStation = new WeatherStation();
+
+let tempDisplay = new TemperatureDisplay(weatherStation);
+let fan = new Fan(weatherStation);
+
+
+weatherStation.setTemperature(20);
+weatherStation.setTemperature(30);
